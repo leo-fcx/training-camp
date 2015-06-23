b5710b168efab521cbebf59dd43c4a0b7fda6bfb
Leo <leo.fcx@gmail.com>
2015-06-22 21:29:32 -0400

    Adding config changes

--- both/accounts/config.js
+++ both/accounts/config.js
@@ -1,3 +1,18 @@
+AccountsTemplates.configure({
+    // Behaviour
+    enablePasswordChange: true,
+    showForgotPasswordLink: true,
+    sendVerificationEmail: true,
+    showResendVerificationEmailLink: true
+});
+
 AccountsTemplates.configureRoute('signIn', {layoutTemplate: 'appLayout'});
 AccountsTemplates.configureRoute('signUp', {layoutTemplate: 'appLayout'});
 AccountsTemplates.configureRoute('ensureSignedIn', {layoutTemplate: 'appLayout'});
+AccountsTemplates.configureRoute('changePwd');
+AccountsTemplates.configureRoute('enrollAccount');
+AccountsTemplates.configureRoute('forgotPwd');
+// AccountsTemplates.configureRoute('hide');
+// AccountsTemplates.configureRoute('resendValidationEmail');
+AccountsTemplates.configureRoute('resetPwd');
+AccountsTemplates.configureRoute('verifyEmail');
